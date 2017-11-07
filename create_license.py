59a84e43040ca933da6fa92c1142728c22541dca
allsway <ssinick@calstate.edu>
2017-11-06 16:13:38 -0800

    Updated logging for downloadable error file

--- create_license.py
+++ create_license.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3.5
 
 import sys
+import csv
 import pandas as pd
 import numpy as np
 import xlrd
-import csv
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
 import json
 import dateparser
+import datetime
 import configparser
 import logging
 import requests
+import re
+import xml.etree.ElementTree as ET
 
 # Returns the API key
 def get_key():
@@ -29,23 +35,37 @@ def post_license(license):
     if r.status_code == 200:
         logging.info('Success update for: ' + url)
     else:
+        check_for_errors(r.content,license['code'])
         logging.info('Failed to post: ' + license['code'])
 
 # Creates license JSON data
 def make_license(row):
     start_date = dateparser.parse(row[5]).strftime('%Y-%m-%d')
+    if re.match('[\d]+$',row[4]):
+        licensor  = '000000' + row[4]
+    else:
+        licensor = row[4]
+    print (licensor)
     license = {
         'code' : row[0],
         'name' : row[1],
         'type' : {'value' : row[2].upper(), 'desc' : row[2]},
         'status': {'value': row[3].upper(), 'desc' : row[3]},
-        'licensor' : {'value' : row[4].upper(), 'desc': row[4]},
+        'licensor' : {'value' : licensor.upper(), 'desc': licensor},
         'location' : {'value' : row[6].upper(), 'desc' : row[5]},
         'start_date': start_date + 'Z',
         'review_status': {'value': row[7].upper(), 'desc' : row[7]}
     }
     print (json.dumps(license))
     post_license(license)
+
+# Create error log file for downlaod
+def check_for_errors(license,code):
+    xml = ET.fromstring(license)
+    for errorlist in xml.find("{http://com/exlibris/urm/general/xmlbeans}errorList"):
+        for error in errorlist.findall("{http://com/exlibris/urm/general/xmlbeans}errorMessage"):
+            with open("error_log.csv", "a") as myfile:
+                myfile.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + ' ' +  code + ', ' + error.text + '\n')
 
 # Read new licenses csv file
 def read_csv(licenses):
@@ -59,7 +79,7 @@ def read_csv(licenses):
         f.close()
 
 # Get logging and configuration
-logging.basicConfig(filename='status.log',level=logging.DEBUG)
+logging.basicConfig(filename='error_report.log',level=logging.DEBUG)
 config = configparser.ConfigParser()
 config.read(sys.argv[1])
 
