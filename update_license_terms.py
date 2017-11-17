38905e1afd323eb1cc81988ba5a77a4e5e719684
allsway <ssinick@calstate.edu>
2017-11-16 17:05:08 -0800

    cleaned up logging

--- update_license_terms.py
+++ update_license_terms.py
@@ -2,7 +2,6 @@
 
 import sys
 import csv
-import json
 import logging
 import configparser
 import pandas as pd
@@ -10,6 +9,8 @@ import numpy
 import requests
 import xml.etree.ElementTree as ET
 import datetime
+import html
+import urllib.parse
 
 # Returns the API key
 def get_key():
@@ -22,57 +23,61 @@ def get_base_url():
 # Places PUT request for license to Alma
 def put_license(license,code):
     url = get_base_url() + '/acq/licenses/' + code + '?apikey=' + get_key()
-    headers = {"Content-Type": "application/json"}
-    print (json.dumps(license))
-    r = requests.put(url,data=json.dumps(license),headers=headers)
-    print (r.content)
+    headers = {"Content-Type": "application/xml"}
+    r = requests.put(url,data=ET.tostring(license),headers={"Content-Type" : "application/xml"})
     if r.status_code == 200:
         logging.info('Success update for: ' + url)
     else:
-        check_for_errors(json.loads(r.content.decode('utf-8')),code)
         logging.info('Failed to post: ' + code)
 
-# Returns the JSON formatted license
+# Returns the XML formatted license
 def get_license(code):
-    url = get_base_url() + '/acq/licenses/' + code + '?apikey=' + get_key() + '&format=json'
-    print (url)
+    url = get_base_url() + '/acq/licenses/' + code + '?apikey=' + get_key()
     r = requests.get(url)
     if r.status_code != 200:
         logging.info('Failed to get: ' + url)
     else:
-        return json.loads(r.content.decode('utf-8'))
+        return ET.fromstring(r.content)
 
 # Create error log file for downlaod
 def check_for_errors(license,code):
-    print (license['web_service_result']['errorList']['error']['errorCode'])
+#    print (license['web_service_result']['errorList']['error']['errorCode'])
     with open("error_log.csv", "a") as myfile:
-        myfile.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + ' ' +  code + ', ' + license['web_service_result']['errorList']['error']['errorCode'] + '\n')
+        myfile.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + ' ' +  code + ', ' + license.find('errorList/error/errorMessage').text + '\n')
 
 # Updates the term field from Excel file
 def update_terms(license, terms):
-    term = []
-    """for row in terms[1:].itertuples():
-        values = {'code': {}, 'value' : {}}
+    xml_terms = license.find("terms")
+    for row in terms[1:].itertuples():
+        match_found = False
         if str(row[2]) != 'nan' and str(row[2]) != 'NaN' and str(row[2]) != '' and str(row[2]) != 'Please choose a value':
-            print (str(row[2]))
-            values['code']['value'] =  row[3].upper()
-            values['code']['desc'] = row[3]
-            values['value']['value'] = row[2].upper()
-            values['value']['desc'] = row[2]
-            term.append(values)
-    license['term'] = term"""
-    print (license)
+            if type(row[2]) is str:
+                val = row[2].upper()
+            else:
+                val = str(row[2])
+            for t in xml_terms.findall('term'):
+                if t.find('code').text == row[3].upper():
+                    t.find('value').text = val
+                    match_found = True
+            if not match_found:
+                term = ET.SubElement(xml_terms, 'term')
+                term_code = ET.SubElement(term,'code')
+                term_code.text = row[3].upper()
+                term_code.set('desc',row[3])
+                term_value = ET.SubElement(term,'value')
+                term_value.text = val
     return license
 
 # Read new licenses csv file
 def read_csv(terms):
     df = pd.read_excel(terms)
-    #print (df['LicenseCode'])
     code = df.iloc[0,1]
     license = get_license(code)
     if license is not None:
-        json = update_terms(license, df)
-        put_license(json,code)
+        xml = update_terms(license, df)
+        put_license(xml,code)
+    else:
+        print ('Error')
 
 # Get logging and configuration
 logging.basicConfig(filename='error_report.log',level=logging.DEBUG)
